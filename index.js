155373bb81f18e068bf88ad17889b8ce6c66a3c0
Robson Silva <extern.silva_robson@allianz.com>
2020-09-02 18:21:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let app = express();
+const port = 3000;
+
+//import body parser
+let bodyParser = require('body-parser');
+//import mongoose
+let mongoose = require('mongoose');
+//Import routes
+let apiRoutes = require("./routes")
+
+//Use API routes in the App
+app.use('/api', apiRoutes)
+
+//configure bodyparser to hande the post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+//connect to mongoose
+const dbPath = 'mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true';
+const options = {useNewUrlParser: true, useUnifiedTopology: true}
+const mongo = mongoose.connect(dbPath, options);
+mongo.then(() => {
+    console.log('connected');
+}, error => {
+    console.log(error, 'error');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
